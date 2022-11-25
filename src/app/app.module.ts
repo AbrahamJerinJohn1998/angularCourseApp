2b0b4b6559461e734312e405d0098e124aa3e70b
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-25 17:11:32 +0530

    view courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCoursesComponent } from './add-courses/add-courses.component';
+import { ViewCoursesComponent } from './view-courses/view-courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddCoursesComponent
+    AddCoursesComponent,
+    ViewCoursesComponent
   ],
   imports: [
     BrowserModule,
